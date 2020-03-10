3133e3230c2fa72308dc39fdedf86b7080f08895
Гоша <losetown343@e1.ru>
2020-03-10 23:18:43 +0500

    Updates

---
+++ precache-manifest.d597de1a1b017b64fd6b5d5dbe50e834.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e78f4af1677a98683724543e2be530e",
+    "url": "/task-tracker/index.html"
+  },
+  {
+    "revision": "c37991fe196118c55895",
+    "url": "/task-tracker/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "996006a298be2854ac4c",
+    "url": "/task-tracker/static/css/main.29e1e9ce.chunk.css"
+  },
+  {
+    "revision": "c37991fe196118c55895",
+    "url": "/task-tracker/static/js/2.e8393789.chunk.js"
+  },
+  {
+    "revision": "74dcd8052a644434877c2eb155510a9e",
+    "url": "/task-tracker/static/js/2.e8393789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996006a298be2854ac4c",
+    "url": "/task-tracker/static/js/main.7380c386.chunk.js"
+  },
+  {
+    "revision": "60b95233a7f2e51150cf",
+    "url": "/task-tracker/static/js/runtime-main.184ce14a.js"
+  }
+]);
