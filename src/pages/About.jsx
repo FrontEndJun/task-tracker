5b356782ec279dea5639eea9e319e8333aedf123
Гоша <losetown343@e1.ru>
2020-03-15 21:15:30 +0500

    add description at /about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,12 @@ import React from "react";
 export const About = () => {
   return (
     <div className="container">
-      <h5>Developed By Kondrev Georgy {"<george.kondrev@yandex.ru>"}</h5>
-      <p> Special for Deltaplan</p>
+      <h4>Developed By Kondrev Georgy {"<george.kondrev@yandex.ru>"}</h4>
+
+      <h5>Simple TaskTracker with authorization</h5>
+
       <p> Provided By React - Node.js - Express - MySql </p>
+      <p> Deployed By Heroku - GitHub Pages </p>
     </div>
   );
 };
