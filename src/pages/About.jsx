cc0f209364953acbc192431c4b5e41d0287e96b8
Гоша <losetown343@e1.ru>
2020-03-15 20:57:29 +0500

    fix prev commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="container">
+      <h5>Developed By Kondrev Georgy {"<george.kondrev@yandex.ru>"}</h5>
+      <p> Special for Deltaplan</p>
+      <p> Provided By React - Node.js - Express - MySql </p>
+    </div>
+  );
+};
